62dbf3f0480b0652c6c9718ac06907e63ade5c5f
changzih <769846892@qq.com>
2021-04-05 22:03:51 +0800

    完成了商品功能开发

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ import "./assets/fonts/iconfont.css"
 import axios from 'axios'
 // 导入带有树形表格的插件
 import TreeTable from 'vue-table-with-tree-grid'
+//导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+//导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 
 
 // 配置请求的根据经
@@ -29,6 +36,21 @@ Vue.config.productionTip = false
 
 // 注册带有树形结构的表格的插件
 Vue.component('tree-table', TreeTable)
+
+// 将富文本编辑器注册为全局可用得到组件
+Vue.use(VueQuillEditor)
+
+// 全局时间格式过滤器
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal * 1000)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, Steps, Step, Checkbox, CheckboxGroup, Upload } from 'element-ui'
 
 // 注册为全局可用的组件
 Vue.use(Button)
@@ -33,6 +33,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
 
 
 // 这个和其他的配置方式不一样，需要全局挂载，挂载到vue，$message是个自定义属性，后面的Message是个弹框组件。之后都可以通过this访问$messgae
